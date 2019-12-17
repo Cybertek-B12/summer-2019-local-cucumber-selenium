78b8f021ae2183dd4d2391413e250c7d382729a6
CybertekSchool <github@cybertekschool.com>
2019-12-17 16:52:07 -0500

    added utilities

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -6,11 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
         features = "src/test/resources/features/",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        tags = "@login"
-
+        dryRun = false
 )
 public class CukesRunner {
 }
---
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -0,0 +1,25 @@
+package com.vytrack.step_definitions;
+
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    @Before
+    public void setUp() {
+        System.out.println("\nThis is coming from before scenario");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("This is coming from after scenario\n");
+
+    }
+
+    @After("@sales_manager")
+    public void tearDownSalesManager(){
+        System.out.println("This is coming from after scenario for sales managers\n");
+
+    }
+}
